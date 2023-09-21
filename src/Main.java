2e3cab86ca991eecd872650e286a0fbec9b927f8
Роман <urmatos92@gmai.com>
2023-09-22 01:31:50 +0700

    ДЗ строки

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Привет Имр!");
+        System.out.println("Задание №1");
+        String firsName = "Ivanov";
+        String middleName = "Ivan";
+        String lastName = "Ivanovich";
+        String fullName = firsName + " " + middleName + " " + lastName;
+        System.out.println("Ф.И.О сотрудника - "+fullName);
+        System.out.println("Задание №2");
+        String upperFullName = fullName.toUpperCase();
+        System.out.println(upperFullName);
+        System.out.println("Задание №3");
+        fullName = "Иванов Семён Семёнович";
+        String s1 = fullName.replace("ё", "e");
+        System.out.println("Данные Ф.И.О сотрудника - "+s1);
     }
 }
